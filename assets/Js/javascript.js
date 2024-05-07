5601eefc67ced2ef50262f417325647a88604c8e
Shiraz Ali <shiraz998ali@gmail.com>
2024-05-07 21:35:08 +0500

    done

---
+++ assets/Js/javascript.js
@@ -0,0 +1,22 @@
+//-- Start Js Logo Slider ---- //
+var swiper = new Swiper(".top-slider", {
+  slidesPerView: 7,
+  spaceBetween: 0,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  }
+});
+//-- Close Js Logo Slider ---- //
+//-- Start Js News Slider ---- //
+var swiper = new Swiper(".news-slider", {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+//-- Close Js News Slider ---- //
+
