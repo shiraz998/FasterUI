2531470603df318ce97b0c5022d36df40d5f0a69
Shiraz Ali <shiraz998ali@gmail.com>
2024-05-11 22:07:39 +0500

    done

--- assets/Js/javascript.js
+++ assets/Js/javascript.js
@@ -1,11 +1,29 @@
 //-- Start Js Logo Slider ---- //
 var swiper = new Swiper(".top-slider", {
-  slidesPerView: "auto",
-  spaceBetween: 73,
-  loop: true,
-  // autoplay: {
-  //   delay: 3000,
-  // }
+    slidesPerView: "auto",
+    spaceBetween: 73,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    breakpoints: {
+    320: {
+      // slidesPerView: 1,
+      spaceBetween: 25,
+    },
+    768: {
+      // slidesPerView: 7,
+      spaceBetween: 30,
+    },
+    1024: {
+      // slidesPerView: 7,
+      spaceBetween: 73,
+    },
+    1440: {
+      // slidesPerView: 7,
+      spaceBetween: 73,
+    },
+  }
 });
 //-- Close Js Logo Slider ---- //
 //-- Start Js News Slider ---- //
@@ -17,6 +35,24 @@ var swiper = new Swiper(".news-slider", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 25,
+      },
+      1440: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+      },
+    }
 });
 //-- Close Js News Slider ---- //
 
